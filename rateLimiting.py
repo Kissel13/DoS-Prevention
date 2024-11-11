362bceb273a86a15bb920cc547d75a85a4d9de0e
Chance <ctkiscool13@gmail.com>
2024-11-11 12:22:15 -0800

    simple algorithm layout

---
+++ rateLimiting.py
@@ -0,0 +1,15 @@
+class FixedWindow:
+    def __init__(self, window, requestAllowed):
+        #define the size of our accepting window
+        #define the maxmimum number of requests we want to allow
+        #define a variable for tracking requests recieved
+        #define a variable for determining start of window
+        window = 
+        requestAllowed = 
+        
+    def request_handler():
+        #check for whether or not the given request is allowed under the limitations
+        #if it is under the amount
+            #allow through
+        #else 
+            #deny the request
